52b5a871ccc78db7d52be98a86c8be7235fb3625
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-07-01 19:05:50 +0800

    refactoring: inline selenium web driver class

--- src/cucumber/java/com/odde/hangman/driver/ConfigurableDriver.java
+++ src/cucumber/java/com/odde/hangman/driver/ConfigurableDriver.java
@@ -1,8 +1,19 @@
 package com.odde.hangman.driver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Component
 @Scope("cucumber-glue")
@@ -10,43 +21,77 @@ public class ConfigurableDriver implements Driver {
 
     private static final String DELIMITER = ":";
     private static final String HOST_NAME = "http://localhost";
-    private final Driver originalDriver = new SeleniumWebDriver();
+    private static final int DEFAULT_TIME_OUT_IN_SECONDS = 10;
+    private final WebDriver webDriver = new FirefoxDriver();
+
+    @Override
+    public void close() {
+        webDriver.close();
+    }
+
+    @Override
+    public void navigateTo(String url) {
+        webDriver.get(urlWithHostAndPort(url));
+    }
+
+    @Override
+    public void waitForTextPresent(String text) {
+        new WebDriverWait(webDriver, DEFAULT_TIME_OUT_IN_SECONDS).until(
+                (ExpectedCondition<Boolean>) webDriver -> getAllTextInPage().contains(text));
+    }
+
+    @Override
+    public void inputTextByName(String text, String name) {
+        elementByName(name).sendKeys(text);
+    }
+
+    private WebElement elementByName(String name) {
+        return webDriver.findElement(By.name(name));
+    }
+
+    @Override
+    public void clickByText(String text) {
+        firstElementByText(text).click();
+    }
+
+    private WebElement firstElementByText(String text) {
+        return elementsByText(text)
+                .findFirst().<NoSuchElementException>orElseThrow(() -> {
+                    throw new NoSuchElementException(String.format("no element can be found by text: %s", text));
+                });
+    }
+
+    private Stream<WebElement> elementsByText(String text) {
+        return Stream.of(
+                elementsByXPath(String.format("//input[@value='%s']", text)),
+                elementsByXPath(String.format("//button[text()='%s']", text)),
+                elementsByLinkText(text))
+                .flatMap(Collection::stream);
+    }
+
+    private List<WebElement> elementsByLinkText(String text) {
+        return webDriver.findElements(By.linkText(text));
+    }
+
+    private List<WebElement> elementsByXPath(String xpath) {
+        return webDriver.findElements(By.xpath(xpath));
+    }
+
+    @Override
+    public String getAllTextInPage() {
+        return elementByTag().getText();
+    }
+
+    private WebElement elementByTag() {
+        return webDriver.findElement(By.tagName("body"));
+    }
+
     @Value("${cucumber.port}")
     private String port;
     @Value("${cucumber.context-path}")
     private String contextPath;
 
-    @Override
-    public void close() {
-        originalDriver.close();
-    }
-
-    @Override
-    public void navigateTo(String url) {
-        originalDriver.navigateTo(urlWithHostAndPort(url));
-    }
-
     private String urlWithHostAndPort(String url) {
         return HOST_NAME + DELIMITER + port + contextPath + url;
-    }
-
-    @Override
-    public void waitForTextPresent(String text) {
-        originalDriver.waitForTextPresent(text);
-    }
-
-    @Override
-    public void inputTextByName(String text, String name) {
-        originalDriver.inputTextByName(text, name);
-    }
-
-    @Override
-    public void clickByText(String text) {
-        originalDriver.clickByText(text);
-    }
-
-    @Override
-    public String getAllTextInPage() {
-        return originalDriver.getAllTextInPage();
     }
 }
