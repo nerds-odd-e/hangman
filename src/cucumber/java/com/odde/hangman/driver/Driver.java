e2cd9dc1dfc0fd2faa18af9396d49a0edec829e5
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-07-31 10:07:02 +0800

    refactoring: replace driver interface with the class

--- src/cucumber/java/com/odde/hangman/driver/Driver.java
+++ src/cucumber/java/com/odde/hangman/driver/Driver.java
@@ -1,15 +1,90 @@
 package com.odde.hangman.driver;
 
-public interface Driver {
-    void close();
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
-    void navigateTo(String url);
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
 
-    void waitForTextPresent(String text);
+@Component
+@Scope("cucumber-glue")
+public class Driver {
 
-    void inputTextByName(String text, String name);
+    private static final String DELIMITER = ":";
+    private static final String HOST_NAME = "http://localhost";
+    private static final int DEFAULT_TIME_OUT_IN_SECONDS = 10;
+    private final WebDriver webDriver = new ChromeDriver();
+    @Value("${cucumber.port}")
+    private String port;
+    @Value("${cucumber.context-path}")
+    private String contextPath;
 
-    void clickByText(String text);
+    public void close() {
+        webDriver.close();
+    }
 
-    String getAllTextInPage();
+    public void navigateTo(String url) {
+        webDriver.get(urlWithHostAndPort(url));
+    }
+
+    public void waitForTextPresent(String text) {
+        new WebDriverWait(webDriver, DEFAULT_TIME_OUT_IN_SECONDS).until(
+                (ExpectedCondition<Boolean>) webDriver -> getAllTextInPage().contains(text));
+    }
+
+    public void inputTextByName(String text, String name) {
+        elementByName(name).sendKeys(text);
+    }
+
+    private WebElement elementByName(String name) {
+        return webDriver.findElement(By.name(name));
+    }
+
+    public void clickByText(String text) {
+        firstElementByText(text).click();
+    }
+
+    private WebElement firstElementByText(String text) {
+        return elementsByText(text)
+                .findFirst().<NoSuchElementException>orElseThrow(() -> {
+                    throw new NoSuchElementException(String.format("no element can be found by text: %s", text));
+                });
+    }
+
+    private Stream<WebElement> elementsByText(String text) {
+        return Stream.of(
+                elementsByXPath(String.format("//input[@value='%s']", text)),
+                elementsByXPath(String.format("//button[text()='%s']", text)),
+                elementsByLinkText(text))
+                .flatMap(Collection::stream);
+    }
+
+    private List<WebElement> elementsByLinkText(String text) {
+        return webDriver.findElements(By.linkText(text));
+    }
+
+    private List<WebElement> elementsByXPath(String xpath) {
+        return webDriver.findElements(By.xpath(xpath));
+    }
+
+    public String getAllTextInPage() {
+        return elementByTag().getText();
+    }
+
+    private WebElement elementByTag() {
+        return webDriver.findElement(By.tagName("body"));
+    }
+
+    private String urlWithHostAndPort(String url) {
+        return HOST_NAME + DELIMITER + port + contextPath + url;
+    }
 }
