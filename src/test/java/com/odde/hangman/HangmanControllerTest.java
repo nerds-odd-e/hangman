82d46d73ac5083d6e69425148fd1fbe7dd2a1447
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-08-02 21:20:42 +0800

    refactoring: use nested runner to organize home controller tests

--- src/test/java/com/odde/hangman/HangmanControllerTest.java
+++ src/test/java/com/odde/hangman/HangmanControllerTest.java
@@ -1,53 +1,64 @@
 package com.odde.hangman;
 
+import com.nitorcreations.junit.runners.NestedRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.ui.Model;
 
 import static org.mockito.Mockito.*;
 
+@RunWith(NestedRunner.class)
 public class HangmanControllerTest {
 
     Hangman mockHangman = mock(Hangman.class);
     HangmanController controller = new HangmanController(mockHangman);
     Model mockModel = mock(Model.class);
 
-    @Test
-    public void home_should_set_tries_when_start_game() {
-        givenStartGameWithTries(100);
-
-        home();
-
-        verify(mockModel).addAttribute("tries", 100);
-    }
-
-    @Test
-    public void input_should_set_tries_when_input_a_char() {
-        givenStartGameWithTries(100);
-
-        input("a");
-
-        verify(mockModel).addAttribute("tries", 100);
-    }
-
-    @Test
-    public void input_should_invoke_hangman_input_when_a_char() {
-        givenStartGameWithTries(99);
-
-        input("a");
-
-        verify(mockHangman).input("a");
-    }
-
-    private void input(String character) {
-        controller.input(mockModel, character);
-    }
-
     private void givenStartGameWithTries(int value) {
         when(mockHangman.tries()).thenReturn(value);
     }
 
-    private String home() {
-        return controller.home(mockModel);
+    public class Input {
+
+        @Test
+        public void should_set_tries_when_input_a_char() {
+            givenStartGameWithTries(100);
+
+            input("a");
+
+            verify(mockModel).addAttribute("tries", 100);
+        }
+
+        @Test
+        public void should_invoke_hangman_input_when_a_char() {
+            givenStartGameWithTries(99);
+
+            input("a");
+
+            verify(mockHangman).input("a");
+        }
+
+        private void input(String character) {
+            controller.input(mockModel, character);
+        }
+
+    }
+
+    public class Home {
+
+        @Test
+        public void should_set_tries_when_start_game() {
+            givenStartGameWithTries(100);
+
+            home();
+
+            verify(mockModel).addAttribute("tries", 100);
+        }
+
+        private String home() {
+            return controller.home(mockModel);
+        }
+
     }
 
 }
